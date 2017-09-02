2daf006005591fdc530b0295aa90612f854f1d21
DESKTOP-892PSIB\EQUIPO <juanlol94@hotmail.com>
2017-09-01 22:09:57 -0500

    stealth V1

---
+++ Stealth/Assets/scripts/GameUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour {
+
+    public GameObject gameLoseUI;
+    public GameObject gameWinUI;
+    bool gameIsOver;
+
+	// Use this for initialization
+	void Start () {
+        Guard.OnGuardHasSpottedPlayer += ShowGameLoseUI;
+        Guard2.OnGuardHasSpottedPlayer += ShowGameLoseUI;
+        FindObjectOfType<Player>().OnReachedEndOfLevel += ShowGameWinUI;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameIsOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+	}
+
+    void ShowGameWinUI()
+    {
+        OnGameOver(gameWinUI);
+    }
+
+    void ShowGameLoseUI()
+    {
+        OnGameOver(gameLoseUI);
+    }
+
+    void OnGameOver(GameObject gameOverUI)
+    {
+        gameOverUI.SetActive(true);
+        gameIsOver = true;
+        Guard.OnGuardHasSpottedPlayer -= ShowGameLoseUI;
+        FindObjectOfType<Player>().OnReachedEndOfLevel -= ShowGameWinUI;
+    }
+}
